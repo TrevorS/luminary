7ba54a07bb21b5417b9f69afbb48d2b13fed9179
Trevor Strieber <trevor@strieber.org>
2018-06-08 08:08:22 -0700

    WIP bounds.

--- src/core/bounds2.rs
+++ src/core/bounds2.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 use core::point2::Point2;
 use core::value::Value;
 
@@ -8,7 +10,24 @@ pub struct Bounds2<T: Value> {
 }
 
 impl<T: Value> Bounds2<T> {
-    pub fn new() -> Self {
+    pub fn new(p1: Point2<T>, p2: Point2<T>) -> Self {
+        let p_min = Point2{
+            x: p1.x.min(p2.x),
+            y: p1.y.min(p2.y),
+        };
+
+        let p_max = Point2{
+            x: p1.x.max(p2.x),
+            y: p1.y.max(p2.y),
+        };
+
+        Self {
+            p_min: p_min,
+            p_max: p_max,
+        }
+    }
+
+    pub fn zero() -> Self {
         let min_num = T::min_value();
         let max_num = T::max_value();
 
@@ -27,6 +46,19 @@ impl<T: Value> Bounds2<T> {
 
 impl<T: Value> From<Point2<T>> for Bounds2<T> {
     fn from(p: Point2<T>) -> Self {
-        Self { p_min: p, p_max: p }
+        Self::new(p, p)
+    }
+}
+
+impl<T: Value> Index<usize> for Bounds2<T> {
+    type Output = Point2<T>;
+
+    fn index(&self, i: usize) -> &Point2<T> {
+        assert!(i <= 1);
+
+        match i {
+            0 => &self.p_min,
+            _ => &self.p_max,
+        }
     }
 }
--- src/core/bounds3.rs
+++ src/core/bounds3.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 use core::point3::Point3;
 use core::value::Value;
 
@@ -8,7 +10,26 @@ pub struct Bounds3<T: Value> {
 }
 
 impl<T: Value> Bounds3<T> {
-    pub fn new() -> Self {
+    pub fn new(p1: Point3<T>, p2: Point3<T>) -> Self {
+        let p_min = Point3{
+            x: p1.x.min(p2.x),
+            y: p1.y.min(p2.y),
+            z: p1.z.min(p2.z),
+        };
+
+        let p_max = Point3{
+            x: p1.x.max(p2.x),
+            y: p1.y.max(p2.y),
+            z: p1.z.max(p2.z),
+        };
+
+        Self {
+            p_min: p_min,
+            p_max: p_max,
+        }
+    }
+
+    pub fn zero() -> Self {
         let min_num = T::min_value();
         let max_num = T::max_value();
 
@@ -25,10 +46,45 @@ impl<T: Value> Bounds3<T> {
             },
         }
     }
+
+    pub fn corner(self, corner: usize) -> Point3<T> {
+        let x_idx = corner & 1;
+
+        let y_idx = if corner & 2 > 0 {
+            1
+        } else {
+            0
+        };
+
+        let z_idx = if corner & 4 > 0 {
+            1
+        } else {
+            0
+        };
+
+        Point3 {
+            x: self[x_idx].x,
+            y: self[y_idx].y,
+            z: self[z_idx].z,
+        }
+    }
 }
 
 impl<T: Value> From<Point3<T>> for Bounds3<T> {
     fn from(p: Point3<T>) -> Self {
-        Self { p_min: p, p_max: p }
+        Self::new(p, p)
+    }
+}
+
+impl<T: Value> Index<usize> for Bounds3<T> {
+    type Output = Point3<T>;
+
+    fn index(&self, i: usize) -> &Point3<T> {
+        assert!(i <= 1);
+
+        match i {
+            0 => &self.p_min,
+            _ => &self.p_max,
+        }
     }
 }
